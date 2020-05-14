16a4cacddc682dbc800670c5ff5d7d5ba0a7b2ce
RuthMaria <ruth-15rms@hotmail.com>
2020-05-14 02:52:16 -0300

    post and categories are finished

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const flash = require('connect-flash')
 const app = express()
 require('./models/Post')
 const Post = mongoose.model('posts')
+require('./models/Category')
+const Category = mongoose.model('categories')
+const moment = require('moment')
 
 // Configuration 
 app.use(session({
@@ -29,11 +32,18 @@ app.use((request, response, next) => { // middleware with global variables
 app.use(bodyParser.urlencoded({
     extended:true
 }))
+
 app.use(bodyParser.json())
 
 app.engine('handlebars', handlebars({
-    defaultLayout: 'main'
+    defaultLayout: 'main',
+    helpers: {
+        formatDate: (date) => {
+            return moment(date).format('DD/MM/YYYY')
+        }
+    }
 }))
+
 app.set('view engine', 'handlebars')
 
 mongoose.Promise = global.Promise
@@ -48,6 +58,7 @@ mongoose.connect('mongodb://localhost/blogapp', {
     console.log("Error connecting to mongoDB "+err)
 })
 
+
 // Public
 app.use(express.static(path.join(__dirname, "public")))
 
@@ -58,6 +69,48 @@ app.get('/', (request, response) => {
     }).catch((err) => {
         request.flash('error_msg', 'Internal error')
         response.redirect('/404')
+    })
+})
+
+app.get('/post/:slug', (request, response) => {
+    Post.findOne({slug: request.params.slug}).lean().then((post) => {
+        if (post) {
+            response.render('post/index', {post:post})
+        } else {
+            request.flash('error_msg', 'This post does not exist!')
+            response.redirect('/')
+        }
+    }).catch((err) => {
+        request.flash('error_msg', 'Internal error')
+        response.redirect('/')
+    })
+})
+
+app.get('/categories', (request, response) => {
+    Category.find().lean().then((categories) => {
+        response.render('categories/index', {categories:categories})
+    }).catch((err) => {
+        request.flash('error_msg', 'Error listing the category!')
+        response.redirect('/')
+    })
+})
+
+app.get('/categories/:slug', (request, response) => {
+    Category.findOne({slug:request.params.slug}).lean().then((category) => {
+        if (category) {
+            Post.find({category:category._id}).lean().then((posts) => {
+                response.render('categories/posts', {posts:posts, category:category})
+            }).catch((err) => {
+                request.flash('error_msg', 'Error listing the posts')
+                response.redirect('/')
+            })
+        } else {
+            request.flash('error_msg', 'this category does not exist!')
+            response.redirect('/')
+        }
+    }).catch((err) => {
+        request.flash('error_msg', 'Error loading the page this category!')
+        response.redirect('/')
     })
 })
 
